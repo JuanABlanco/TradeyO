525aba05f71a175cc67572cfa639be6f1d0e5d4b
JuanABlanco <35752281+JuanABlanco@users.noreply.github.com>
2018-03-30 11:46:54 -0400

    Conexion a la base de datos de FireBase
    Conectamos la app con FireBase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase{
+    apiKey: "AIzaSyBhBIbIJoTG-rB56-Dw7tOA-1dBdsJLpp4",
+    authDomain: "tradeyo-798f4.firebaseapp.com",
+    databaseURL: "https://tradeyo-798f4.firebaseio.com",
+    projectId: "tradeyo-798f4",
+    storageBucket: "",
+    messagingSenderId: "826447022962"
+  }
 };
